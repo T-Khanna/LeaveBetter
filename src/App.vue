43823fbc22df1b985d48525aa879bf3a279f5169
Martina Jireckova <jirecm@amazon.com>
2022-01-24 14:50:09 +0000

    Add selectable

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@ import Calendar from './components/Calendar.vue'
   </header>
 
   <main>
-    <TheWelcome />
     <Calendar />
   </main>
 </template>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -12,12 +12,34 @@ export default {
     return {
       calendarOptions: {
         plugins: [ dayGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+        { title: 'event 1', date: '2022-01-24' },
+        { title: 'event 2', date: '2022-01-25' }
+        ],
+        weekends: false, // initial value
+        firstDay : 1,
+        editable: true,
+        selectable: true,
+        select: this.handleDateSelect
       }
+    }
+  },
+  methods: {
+   // handleDateClick: function(arg) {
+   //   alert('date click! ' + arg.dateStr)
+   // },
+     toggleWeekends: function() {
+      this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    },
+     handleDateSelect: function(info) {
+      alert('selected ' + info.startStr + ' to ' + info.endStr);
     }
   }
 }
 </script>
 <template>
+  <button @click="toggleWeekends">toggle weekends</button>
   <FullCalendar :options="calendarOptions" />
 </template>
